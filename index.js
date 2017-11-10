fce3bf72e8a7012693b7f60e168393eb873e1e4f
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2017-11-10 23:56:28 +0900

    added gtp-ray

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const { move2coord, coord2move } = require('./gtp-util.js');
 const { InvalidConfiguration, GtpClient } = require('./gtp.js');
 const { GtpLeela } = require('./gtp-leela.js');
+const { GtpRay } = require('./gtp-ray.js');
 
 exports.move2coord = move2coord;
 exports.coord2move = coord2move;
 exports.InvalidConfiguration = InvalidConfiguration;
 exports.GtpClient = GtpClient;
 exports.GtpLeela = GtpLeela;
+exports.GtpRay = GtpRay;
