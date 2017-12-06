9b646f21e472dabf1fd195e3285f3d4a6034376b
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2017-12-06 19:52:08 +0900

    removed gtp-ray

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { move2coord, coord2move } = require('./gtp-util.js');
 const { InvalidConfiguration, GtpBase } = require('./gtp-base.js');
 const { GtpClient } = require('./gtp-client.js');
 const { GtpLeela } = require('./gtp-leela.js');
-const { GtpRay } = require('./gtp-ray.js');
 
 exports.move2coord = move2coord;
 exports.coord2move = coord2move;
@@ -12,4 +11,3 @@ exports.InvalidConfiguration = InvalidConfiguration;
 exports.GtpBase = GtpBase;
 exports.GtpClient = GtpClient;
 exports.GtpLeela = GtpLeela;
-exports.GtpRay = GtpRay;
