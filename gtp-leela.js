3800cbab0581e446d37a0a1aeac36f7d7d2cc7ba
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2018-05-02 10:40:32 +0900

    GtpLeelaZero{19,9}

--- gtp-leela.js
+++ gtp-leela.js
@@ -6,21 +6,15 @@ const { GtpClient } = require('./gtp-client.js');
 class GtpLeela extends GtpClient {
     static init() {
         super.init();
-        if (process.env.LEELA === 'leelaz') {
-            this.WORK_DIR = __dirname + '/Leela-Zero/';
-            this.COMMAND = './leelaz';
-            this.OPTIONS = ['-g', '-w', 'weights.txt'];
-        } else {
-            this.WORK_DIR = __dirname + '/Leela0110GTP/';
-            this.COMMAND = './' + (function() {
-                switch (process.platform) {
-                    case 'linux': return 'leela_0110_linux_x64';
-                    case 'darwin': return 'leela_0110_macOS';
-                    default: throw new Error('not-supported');
-                }
-            })() + (process.env.LEELA === 'opencl' ? '_opencl' : '');
-            this.OPTIONS = ['--gtp'];
-        }
+        this.WORK_DIR = __dirname + '/Leela0110GTP/';
+        this.COMMAND = './' + (function() {
+            switch (process.platform) {
+                case 'linux': return 'leela_0110_linux_x64';
+                case 'darwin': return 'leela_0110_macOS';
+                default: throw new Error('not-supported');
+            }
+        })() + (process.env.LEELA === 'opencl' ? '_opencl' : '');
+        this.OPTIONS = ['--gtp'];
     }
 
     async play(coord) {
@@ -53,7 +47,7 @@ class GtpLeela extends GtpClient {
             this.info.variations.push({
                 move: match[1],
                 rollouts: parseInt(match[2]),
-                winRate: parseInt(match[3]),
+                winRate: parseFloat(match[3]),
                 pv: match[4].trim().split(' ').map(e =>
                 coord2move(e.toUpperCase(), this.size))
             });
@@ -76,10 +70,115 @@ class GtpLeela extends GtpClient {
     }
 }
 
+class GtpLeelaZero extends GtpClient {
+    async play(coord) {
+        this.info = {
+            comment: null,
+            averageDepth: null,
+            maxDepth: null,
+            nonLeafNodes: null,
+            averageChildren: null,
+            visits: null,
+            nodes: null,
+            variations: []
+        };
+        const value = await super.play(coord, this.playStderrHandler);
+        return Object.assign(value, this.info);
+    }
+
+    async genmove(stderrHandler) {
+        this.info = {
+            comment: null,
+            averageDepth: null,
+            maxDepth: null,
+            nonLeafNodes: null,
+            averageChildren: null,
+            visits: null,
+            nodes: null,
+            variations: []
+        };
+        const value = await super.genmove(stderrHandler ? line => {
+            this.genmoveStderrHandler(line);
+            stderrHandler.call(this, line);
+        } : this.genmoveStderrHandler);
+        return Object.assign(value, this.info);
+    }
+
+    playStderrHandler(line) {
+        const match = line.match(/([A-Z][0-9]{1,2}) ->\s*([0-9]+) \(V:\s*([.0-9]+)%\) \(N:\s*([.0-9]+)%\).*PV:((?: [a-zA-Z][0-9]{1,2})+)/);
+        if (match) {
+            this.info.variations.push({
+                move: match[1],
+                rollouts: parseInt(match[2]),
+                winRate: parseFloat(match[3]),
+                policy: parseFloat(match[4]),
+                pv: match[5].trim().split(' ').map(e =>
+                coord2move(e.toUpperCase(), this.size))
+            });
+        } else {
+            const match = line.match(/([0-9\.]+) average depth, ([0-9]+) max depth/);
+            if (match) {
+                this.info.averageDepth = parseFloat(match[1]);
+                this.info.maxDepth = parseInt(match[1]);
+            } else {
+                const match = line.match(/([0-9]+) non leaf nodes, ([0-9\.]+) average children/);
+                if (match) {
+                    this.info.nonLeafNodes = parseInt(match[1]);
+                    this.info.averageChildren = parseFloat(match[2]);
+                } else {
+                    const match = line.match(/([0-9]+) visits, ([0-9]+) nodes/);
+                    if (match) {
+                        this.info.visits = parseInt(match[1]);
+                        this.info.nodes = parseInt(match[2]);
+                    }
+                }
+            }
+        }
+    }
+
+    genmoveStderrHandler(line) {
+        this.playStderrHandler(line);
+    }
+}
+
+class GtpLeelaZero19 extends GtpLeelaZero {
+    static init() {
+        super.init();
+        this.WORK_DIR = __dirname + '/Leela-Zero/';
+        this.COMMAND = './' + (function() {
+            switch (process.platform) {
+                case 'linux': return 'leelaz';
+                case 'darwin': return 'leelaz_macOS';
+                default: throw new Error('not-supported');
+            }
+        })();
+        this.OPTIONS = ['-g', '-w', 'weights.txt'];
+    }
+}
+
+class GtpLeelaZero9 extends GtpLeelaZero {
+    static init() {
+        super.init();
+        this.WORK_DIR = __dirname + '/Leela-Zero9/';
+        this.COMMAND = './' + (function() {
+            switch (process.platform) {
+                case 'linux': return 'leelaz';
+                case 'darwin': return 'leelaz_macOS';
+                default: throw new Error('not-supported');
+            }
+        })();
+        this.OPTIONS = ['-g', '-w', '20-128-6.5.txt'];
+    }
+}
+
 try {
     GtpLeela.init();
+    GtpLeelaZero19.init();
+    GtpLeelaZero9.init();
 } catch (e) {
     console.log(e.message);
 }
 
 exports.GtpLeela = GtpLeela;
+exports.GtpLeelaZero19 = GtpLeelaZero19;
+exports.GtpLeelaZero9 = GtpLeelaZero9;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const { move2coord, coord2move } = require('./gtp-util.js');
 const { InvalidConfiguration, GtpBase } = require('./gtp-base.js');
 const { GtpClient } = require('./gtp-client.js');
-const { GtpLeela } = require('./gtp-leela.js');
+const { GtpLeela, GtpLeelaZero19, GtpLeelaZero9 } = require('./gtp-leela.js');
 
 exports.move2coord = move2coord;
 exports.coord2move = coord2move;
@@ -11,3 +11,5 @@ exports.InvalidConfiguration = InvalidConfiguration;
 exports.GtpBase = GtpBase;
 exports.GtpClient = GtpClient;
 exports.GtpLeela = GtpLeela;
+exports.GtpLeelaZero19 = GtpLeelaZero19;
+exports.GtpLeelaZero9 = GtpLeelaZero9;
